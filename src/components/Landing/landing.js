e11ada8a5d0266a15e4ac0be2bbfcdfd418dbb86
Tama <tamarushin@gmail.com>
2018-08-11 14:54:12 -0700

    starting over

---
+++ src/components/Landing/landing.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+import React from 'react';
+import React from 'react';
+
+class Landing extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Notes aren't just for MUSIC, ya know!</h1>
+        <p className='intro'></p>
+      </div>
+    );
+
+  }
+}
+
+export default Landing;
---
+++ src/components/Note-Item/note-item.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+class NoteItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      
+    )
