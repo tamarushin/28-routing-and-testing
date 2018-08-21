c18bfecef37b24149bd4ec70cae1322bdc2c454b
Tama <tamarushin@gmail.com>
2018-08-21 16:24:13 -0700

    rendering

--- src/components/Note-List/note-list.js
+++ src/components/Note-List/note-list.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-export default class NoteItem extends Component {
+export default class NoteList extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      this.props.note.map(note => {
+      this.props.notes.map(note => {
         <li key={note.id}>
           <h3>{note.title}</h3>
           <p>{note.content}</p>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import './styles/index.scss';
+import './styles/index.css';
 
 
 import App from './components/App/App';
